2e34eb23fed03dae1514126a30352a79309a0368
Sage Raflik <neonfuz@gmail.com>
2022-03-03 12:54:12 -0600

    Added character chart view to quiz temporarily

--- src/lib/Quiz.svelte
+++ src/lib/Quiz.svelte
@@ -8,11 +8,12 @@
     {#await getlang(lang)}
         Loading...
     {:then zis}
-        {#each zis as zi}
-            <div class="question">
-                <tooltip title={zi.ICAO}>{zi.Upper}</tooltip>
-            </div>
-        {/each}
+        <div class="zi-chart">
+            {#each zis as zi}
+                <tooltip class="question" title={zi.ICAO}>{zi.Upper}</tooltip>
+                {' '}
+            {/each}
+        </div>
         <Textfield />
     {:catch err}
         <div class="error">{err}</div>
@@ -24,6 +25,14 @@
  .question {
      text-align: center;
      font-size: 1.5rem;
+ }
+ .zi-chart {
+     max-width: 95vw;
+     padding: 1em;
+     border-radius: 1em;
+     box-shadow:
+         inset 2px 2px 5px #0001,
+         inset -2px -2px 5px #0001;
  }
  .Quiz :global(input) {
      text-align: center !important;
