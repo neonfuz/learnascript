67bf7c5d9bc37fd25eb00a29a4805cc94454eb92
Sage Raflik <neonfuz@gmail.com>
2022-03-03 18:53:14 -0600

    Added basic usable quiz

--- src/lib/Quiz.svelte
+++ src/lib/Quiz.svelte
@@ -1,10 +1,21 @@
 <script>
  import { Textfield } from 'svelte-mui';
  export let zis;
+ let zi = zis[Math.floor(zis.length * Math.random())];
+ let input = '';
+ $: if (input == zi.ICAO) {
+     zi = zis[Math.floor(zis.length * Math.random())];
+     input = '';
+ }
 </script>
 
 <div class="Quiz">
-    <Textfield />
+    <div class="question">
+        <tooltip title={zi.ICAO}>
+            {zi.Upper}
+        </tooltip>
+    </div>
+    <Textfield bind:value={input} />
 </div>
 
 <style>
