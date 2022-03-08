a4cb7c76d78cab9fa7ee5de3b1b7927aae5cc215
Sage Raflik <neonfuz@gmail.com>
2022-03-08 12:57:00 -0600

    Modified layout styling

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,15 +3,26 @@
  import './style.css';
 </script>
 
-<section>
+<header>
     <h1><a href="/">LEARN A SCRIPT . XYZ</a></h1>
-</section>
+</header>
 
-<slot/>
+<main>
+    <slot/>
+</main>
 
 <WIP/>
 
 <style>
+ header,
+ main > :global(section) {
+     background: var(--box-color);
+     margin-top: 1.5em;
+     padding: 1em;
+     border-radius: .5em;
+     box-shadow: var(--box-shadow);
+     max-width: calc(100vw - 4em);
+ }
  a {
      color: black;
      text-decoration: none;
