0172020e130db1b4ddfb3bdc1876fd5d2fbf4428
Sage Raflik <neonfuz@gmail.com>
2022-03-09 22:50:31 -0600

    Improved touch tooltip

--- src/lib/Chart/JP.svelte
+++ src/lib/Chart/JP.svelte
@@ -18,13 +18,43 @@
  $: rows = new Array(5).fill(0).map((_, row) => (
      layout.filter((item, i) => i%5 === row)
  ));
+ let info;
+ let infoStyle = 'left:0;top:0';
+ function enter(newInfo) {
+     return e => {
+         if (!e.isPrimary) return;
+         info = newInfo;
+         const rect = e.target.getBoundingClientRect();
+         infoStyle = `
+             left: ${rect.x + rect.width/2}px;
+             top:  ${rect.y - rect.height - 4}px;
+         `;
+     }
+ }
+ function leave(e) {
+     if (!e.isPrimary) return;
+     info = undefined;
+ }
 </script>
+
+{#if info}
+    <div id="info" style={infoStyle}>
+        <div>
+            {info}
+        </div>
+        <svg viewBox="0 0 10 10" width=".5em">
+            <path fill="white" stroke="#ccc"
+                  d="M 0,0 L 5,5 L 10,0" />
+        </svg>
+
+    </div>
+{/if}
 
 <table>
     {#each rows as row}
         <tr>
             {#each row as c}
-                <td>
+                <td on:pointerenter={enter(c?.roman)} on:pointerleave={leave}>
                     {c ? c.char : ''}
                 </td>
             {/each}
@@ -33,8 +63,26 @@
 </table>
 
 <style>
+ * {
+     font-size: min(4vw, 2rem);
+ }
+ #info {
+     position: fixed;
+     pointer-events: none;
+     translate: -50%;
+     display: flex;
+     flex-direction: column;
+     align-items: center;
+ }
+ #info div:first-child {
+     background: white;
+     outline: solid #ccc .06em;
+     border-radius: .25em;
+     padding: .4em;
+ }
  table {
      width: 100%;
+     max-width: 300px;
  }
  td {
      padding: .4em;
