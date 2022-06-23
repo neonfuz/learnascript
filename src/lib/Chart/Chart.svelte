9f66a10d3514c4ddffd02510366f05ce97a1a06e
neonfuz <neonfuz@gmail.com>
2022-06-23 16:21:56 -0500

    chart: dropped svelte-loadable (broken on netlify)

--- src/lib/Chart/Chart.svelte
+++ src/lib/Chart/Chart.svelte
@@ -1,20 +1,15 @@
 <script>
- import Loadable from 'svelte-loadable';
+ import Euro from './Euro.svelte';
+ import JP from './JP.svelte';
  export let chars;
  export let lang;
  export let info;
  const pathMap = new Map([
-     ['russian', 'Euro'],
-     ['hiragana', 'JP'],
-     ['katakana', 'JP'],
+     ['russian', Euro],
+     ['hiragana', JP],
+     ['katakana', JP],
  ])
- function getComponentPath(lang) {
-     const name = pathMap.get(lang) ?? 'Json';
-     return `./${name}.svelte`
- }
- $: path = getComponentPath(lang);
+ $: component = pathMap.get(lang);
 </script>
 
-<Loadable loader={() => import(path)} let:component>
-    <svelte:component this={component} {chars} bind:info />
-</Loadable>
+<svelte:component this={component} {chars} bind:info />
